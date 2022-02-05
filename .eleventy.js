97dd65cfc2991dc9bf52d32f8b39cc8f1ca5c62e
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2022-02-05 08:22:24 +0000

    Implement parseCollection filter

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
 
     eleventyConfig.addFilter("jsonify", JSON.stringify);
+    eleventyConfig.addFilter("parseCollection", function(obj) {
+        return obj.map(function(element) {
+            output = {};
+            for (const [key, value] of Object.entries(element.data)) {
+                if(!["site", "types", "eleventy", "page", "collections", "content", "layoutContent", "_layoutContent"].includes(key)) {
+                    output[key] = value;
+                }
+            }
+            return output;
+        });
+    });
 
     eleventyConfig.addPassthroughCopy("static");
 
