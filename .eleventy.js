1ff4b25c43e6961422956a1693989f69dc2219d2
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2022-02-05 00:34:17 +0000

    Support YML data types

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+
     eleventyConfig.addFilter("jsonify", JSON.stringify);
 
     eleventyConfig.addPassthroughCopy("static");
