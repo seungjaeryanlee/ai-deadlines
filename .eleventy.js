386430b40abf4d8eea2368422ba7ad8970e6b351
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2022-02-05 00:23:09 +0000

    Passthrough static/ dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("jsonify", JSON.stringify);
 
+    eleventyConfig.addPassthroughCopy("static");
+
     return {
         dir: {
             includes: "_includes",
